42bbeaedb6352c0f2f16f5c2f346a627087ce9c8
UnnamedHero <hero.nospam@gmail.com>
2018-07-02 17:35:26 +0300

    test write access

--- __tests__/test.js
+++ __tests__/test.js
@@ -1,3 +1,12 @@
-test('example', () => {
+// import os from 'os';
+// import fs from 'mz/fs';
+import pageLoader from '../src';
 
+describe('directory access testing', () => {
+  // const tmpDir = os.tmpdir();
+  expect.assertions(1);
+  test('root dir', async () => {
+    const data = await (pageLoader('jopa', '/'));
+    expect(data.message).toBe('EACCES: permission denied, access \'/\'');
+  });
 });
