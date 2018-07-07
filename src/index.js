64e2a852add035f61601bb0e19490db735a8f4a3
UnnamedHero <hero.nospam@gmail.com>
2018-07-07 10:33:07 +0300

    refactor promise flow

--- src/index.js
+++ src/index.js
@@ -7,94 +7,97 @@ import {
   makePageHelper, getLocalPageLinks, changeLocalResourcesSourceTo, getPageHtml,
 } from './helpers/page';
 
-const getLinks = data => new Promise((resolve) => {
-  const { response } = data;
-  const pageHelper = makePageHelper(response.data);
+const getPageHelper = response => new Promise((resolve) => {
+  resolve(makePageHelper(response.data));
+});
+
+const getLinks = pageHelper => new Promise((resolve) => {
   const links = getLocalPageLinks(pageHelper);
-  resolve({ ...data, links, pageHelper });
+  resolve({ links, pageHelper });
 });
 
-const modifyPageLinks = data => new Promise((resolve) => {
-  const { pageName, pageHelper } = data;
-  const localResourcesPath = `${pageName}_files`;
-  changeLocalResourcesSourceTo(pageHelper, localResourcesPath);
-  resolve({ ...data, pageHelper });
-});
-
-const writeHtml = data => new Promise((resolve, reject) => {
-  const { pageHelper, outputHtmlFile } = data;
-  const html = getPageHtml(pageHelper);
-  fs.writeFile(outputHtmlFile, html)
-    .then(() => resolve(data))
-    .catch(e => reject(e));
-});
-
-const getResourceFileNames = data => new Promise((resolve) => {
-  const { pageHelper } = data;
-  const resourceFileNames = getLocalPageLinks(pageHelper);
-  resolve({ ...data, resourceFileNames });
-});
-
-const createResourceDir = data => new Promise((resolve, reject) => {
-  const { destinationDir, pageName } = data;
-  const resourseDir = path.join(destinationDir, `${pageName}_files`);
-  fs.mkdir(resourseDir)
-    .then(() => resolve(data))
-    .catch(e => reject(e));
-});
-
-const downloadRemoteResources = (data) => {
-  const {
-    links, targetUrl, resourceFileNames, destinationDir,
-  } = data;
-  const downloads = links.map((link, index) => new Promise((resolve, reject) => {
-    const uri = url.resolve(targetUrl, link);
-    const filePath = path.join(destinationDir, resourceFileNames[index]);
-    return axios.get(uri, { responseType: 'stream' })
-      .then(response => resolve({ response, uri, filePath }))
-      .catch(e => reject(e));
-  }));
-  return Promise.all(downloads);
+const modifyPageLinks = (pageHelperAndLinks, targetUrl, destinationDir) => {
+  const pageHelperAndLinksObject = new Promise((resolve) => {
+    const { pageHelper, links } = pageHelperAndLinks;
+    const pageName = makeNameFromUrl(targetUrl);
+    const localResourcesPath = `${pageName}_files`;
+    changeLocalResourcesSourceTo(pageHelper, localResourcesPath);
+    const linkslocalPaths = getLocalPageLinks(pageHelper);
+    const linksObject = links.map((link, index) => {
+      const linkUrl = url.resolve(targetUrl, link);
+      const linklocalPath = path.join(destinationDir, linkslocalPaths[index]);
+      return ({ linkUrl, linklocalPath });
+    });
+    resolve({ pageHelper, linksObject });
+  });
+  return pageHelperAndLinksObject;
 };
 
-const saveDownloadedResources = downloads => Promise.all(downloads
-  .map((download) => {
-    const { response, uri, filePath } = download;
-    return new Promise((resolve, reject) => {
-      response.data.pipe(fs.createWriteStream(filePath)
-        .on('finish', () => resolve({ uri, filePath }))
-        .on('error', e => reject(e)));
+const writeHtml = (pageHelperAndLinksObject, outputHtmlFile) => {
+  const { pageHelper, linksObject } = pageHelperAndLinksObject;
+  const html = getPageHtml(pageHelper);
+  return fs.writeFile(outputHtmlFile, html)
+    .then(() => linksObject)
+    .catch((e) => {
+      throw new Error(e);
     });
-  }));
+};
 
-const notifyResourcesSaved = data => data
-  .forEach(({ uri, filePath }) => {
-    console.log(`'${uri}' was downloaded as ${filePath}`);
+const createResourceDir = (linksObject, destinationDir) => fs.mkdir(destinationDir)
+  .then(() => linksObject)
+  .catch((e) => {
+    throw new Error(e);
   });
 
+const downloadRemoteResources = linksObject => Promise.all(linksObject
+  .map((link) => {
+    const { linkUrl } = link;
+    // missing resource should not fail all mission
+    return new Promise(resolve => resolve(axios.get(linkUrl, { responseType: 'stream' })
+      .then(response => ({ response, ...link }))
+      .catch((e) => {
+        const errorMessage = `${e.message} for '${linkUrl}'`;
+        return ({ error: errorMessage, ...link });
+      })));
+  }));
+
+const saveDownloadedResources = downloads => Promise.all(downloads
+  .map(download => new Promise((resolve, reject) => {
+    const {
+      response, linkUrl, linklocalPath,
+    } = download;
+    if (!download.error) {
+      response.data.pipe(fs.createWriteStream(linklocalPath)
+        .on('finish', () => resolve(`'${linkUrl}' was saved to '${linklocalPath}'`))
+        .on('error', e => reject(new Error(`'${e} Unable to save ${linkUrl}' to '${linklocalPath}'`))));
+    } else {
+      resolve(download.error);
+    }
+  })));
+
+const notifyResourcesSaved = downloadResults => downloadResults
+  .forEach((result) => {
+    console.log(result);
+  });
 
 const pageLoader = (targetUrl, destinationDir) => {
   const pageName = makeNameFromUrl(targetUrl);
   const htmlName = `${pageName}.html`;
-  const outputHtmlFile = path.join(destinationDir, htmlName);
+  const htmlFilePath = path.join(destinationDir, htmlName);
+  const resourceDirPath = path.join(destinationDir, `${pageName}_files`);
   return fs.access(destinationDir, fs.constants.W_OK)
-    .then(() => fs.open(outputHtmlFile, 'wx'))
+    .then(() => fs.open(htmlFilePath, 'wx'))
     .then(() => axios.get(targetUrl))
-    .then(response => new Promise((resolve) => {
-      resolve({
-        response, pageName, destinationDir, targetUrl, outputHtmlFile,
-      });
-    }))
-    .then(data => getLinks(data))
-    .then(data => modifyPageLinks(data))
-    .then(data => writeHtml(data))
-    .then(data => getResourceFileNames(data))
-    .then(data => createResourceDir(data))
-    .then(data => downloadRemoteResources(data))
+    .then(response => getPageHelper(response))
+    .then(pageHelper => getLinks(pageHelper))
+    .then(pageHelperAndLinks => modifyPageLinks(pageHelperAndLinks, targetUrl, destinationDir))
+    .then(pageHelperAndLinksObject => writeHtml(pageHelperAndLinksObject, htmlFilePath))
+    .then(linksObject => createResourceDir(linksObject, resourceDirPath))
+    .then(linksObject => downloadRemoteResources(linksObject))
     .then(downloads => saveDownloadedResources(downloads))
-    .then(data => notifyResourcesSaved(data))
+    .then(downloadResults => notifyResourcesSaved(downloadResults))
     .then(() => {
-      console.log(`Page was downloaded to ${outputHtmlFile}`);
+      console.log(`Page was downloaded to ${htmlFilePath}`);
     })
     .catch((err) => {
       throw new Error(err);
